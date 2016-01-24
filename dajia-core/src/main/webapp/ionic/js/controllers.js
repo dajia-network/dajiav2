eb5c9ca99aeceb8d102bcbec681b37dc8c8640ab
benitoro <nicepuffy@gmail.com>
2016-01-25 00:28:42 +0800

    add login page

--- dajia-core/src/main/webapp/ionic/js/controllers.js
+++ dajia-core/src/main/webapp/ionic/js/controllers.js
@@ -9,35 +9,43 @@ angular.module('starter.controllers', [ "ui.bootstrap", "countTo" ]).controller(
 	// $scope.products = Mocks.getProducts();
 })
 
-.controller('ProdDetailCtrl', function($scope, $stateParams, $state, $window, $timeout, $http, $ionicSlideBoxDelegate) {
-	console.log('产品详情...')
-	// var product = Mocks.getProduct($stateParams.pid);
-	$http.get('/product/' + $stateParams.pid).success(function(data, status, headers, config) {
-		// console.log(data);
-		var product = data;
-		$scope.product = product;
-		$ionicSlideBoxDelegate.update();
-		$scope.orderNeeded = product.maxOrder - product.orderNum;
-		$scope.nextPriceOff = product.priceOff / product.maxOrder;
-		$scope.buyNow = function() {
-			$window.location.href = '#/tab/prod/' + $stateParams.pid + '/order';
-		}
-		var amt = (product.originalPrice - product.currentPrice) / (product.originalPrice - product.targetPrice) * 100;
-		$scope.countTo = product.currentPrice;
-		$scope.countFrom = product.originalPrice;
-		$timeout(function() {
-			$scope.progressValue = amt;
-		}, 1000);
-	}).error(function(data, status, headers, config) {
-		console.log('request failed...');
-	});
-	$scope.progressValue = 0;
-})
+.controller(
+		'ProdDetailCtrl',
+		function($scope, $stateParams, $state, $window, $timeout, $http, $ionicSlideBoxDelegate, $ionicModal) {
+			console.log('产品详情...')
+			loginModalInit($scope, $ionicModal);
+			$http.get('/product/' + $stateParams.pid).success(
+					function(data, status, headers, config) {
+						// console.log(data);
+						var product = data;
+						$scope.product = product;
+						$ionicSlideBoxDelegate.update();
+						$scope.orderNeeded = product.maxOrder - product.orderNum;
+						$scope.nextPriceOff = product.priceOff / product.maxOrder;
+						var amt = (product.originalPrice - product.currentPrice)
+								/ (product.originalPrice - product.targetPrice) * 100;
+						$scope.countTo = product.currentPrice;
+						$scope.countFrom = product.originalPrice;
+						$scope.buyNow = function() {
+							// $window.location.href = '#/tab/prod/' +
+							// $stateParams.pid + '/order';
+							$scope.openModal();
+						}
+						$timeout(function() {
+							$scope.progressValue = amt;
+						}, 1000);
+					}).error(function(data, status, headers, config) {
+				console.log('request failed...');
+			});
+			$scope.progressValue = 0;
+		})
 
-.controller('ProgCtrl', function($scope, Mocks) {
+.controller('ProgCtrl', function($scope, $ionicModal, $timeout, Mocks) {
 	console.log('进度列表...');
-	// $scope.$on('$ionicView.enter', function(e) {
-	// });
+	loginModalInit($scope, $ionicModal);
+	$timeout(function() {
+		$scope.openModal();
+	}, 500);
 	var orders = Mocks.getMyOrders();
 	orders.forEach(function(o) {
 		o.product = Mocks.getProduct(o.pid);
@@ -69,4 +77,36 @@ angular.module('starter.controllers', [ "ui.bootstrap", "countTo" ]).controller(
 	orderItems.push(product);
 	$scope.orderItems = orderItems;
 	$scope.totalPrice = product.price;
+})
+
+.controller('LoginCtrl', function($scope, $ionicModal) {
+	console.log('用户登录...');
+
 });
+
+var loginModalInit = function($scope, $ionicModal) {
+	$ionicModal.fromTemplateUrl('templates/login.html', {
+		scope : $scope,
+		animation : 'slide-in-up'
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
+	$scope.openModal = function() {
+		$scope.modal.show();
+	};
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+	};
+	// Cleanup the modal when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.modal.remove();
+	});
+	// Execute action on hide modal
+	$scope.$on('modal.hidden', function() {
+		// Execute action
+	});
+	// Execute action on remove modal
+	$scope.$on('modal.removed', function() {
+		// Execute action
+	});
+}
