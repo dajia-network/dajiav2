9da03409f201c61ab4d4ba4cef7cfa9b57eba208
benitoro <nicepuffy@gmail.com>
2016-04-10 04:05:54 +0800

    wechat share

--- dajia-core/src/main/webapp/app/js/controllers.js
+++ dajia-core/src/main/webapp/app/js/controllers.js
@@ -103,17 +103,19 @@ angular.module('starter.controllers', [ "ui.bootstrap", "countTo" ]).controller(
 							console.log(res);
 						}
 					});
-					wx.onMenuShareAppMessage({
-						title : 'test', // 分享标题
-						desc : '', // 分享描述
-						link : '/', // 分享链接
-						imgUrl : '', // 分享图标
-						success : function() {
-							// 用户确认分享后执行的回调函数
-						},
-						cancel : function() {
-							// 用户取消分享后执行的回调函数
-						}
+					wx.ready(function() {
+						wx.onMenuShareAppMessage({
+							title : '打价网', // 分享标题
+							desc : $scope.product.name, // 分享描述
+							link : '#', // 分享链接
+							imgUrl : './img/logo.png', // 分享图标
+							success : function() {
+								// 用户确认分享后执行的回调函数
+							},
+							cancel : function() {
+								// 用户取消分享后执行的回调函数
+							}
+						});
 					});
 				});
 			}
