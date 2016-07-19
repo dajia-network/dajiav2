5216e0c71cbcb8c3581369a5e4f265060dbd007c
benitoro <nicepuffy@gmail.com>
2016-07-20 00:17:00 +0800

    bug fixes

--- dajia-core/scripts/db/tables.sql
+++ dajia-core/scripts/db/tables.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS dajia.user (
 	oauth_type VARCHAR(50) NULL,
 	oauth_user_id VARCHAR(50) NULL,
 	last_visit_date TIMESTAMP NULL,
-	last_visit_ip VARCHAR(20) NULL,
+	last_visit_ip VARCHAR(200) NULL,
 	created_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
 	modified_date TIMESTAMP NULL,
     is_active VARCHAR(5) NOT NULL DEFAULT 'Y',
