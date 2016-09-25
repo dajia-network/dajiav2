950a8f78682f29341badbc6983c7eef99fa19fa9
huhaonan <huhaonan.en@gmail.com>
2016-09-25 20:39:12 +0800

    修改产品过期时间任务为1分钟

--- dajia-core/src/main/java/com/dajia/task/ScheduledTasks.java
+++ dajia-core/src/main/java/com/dajia/task/ScheduledTasks.java
@@ -32,7 +32,7 @@ public class ScheduledTasks {
 	@Autowired
 	private RefundService refundService;
 
-	@Scheduled(cron = "0 */10 *  * * * ")
+	@Scheduled(cron = "0 */1 * * * * ")
 	public void productUpdateByCron() {
 		Date currentDate = new Date();
 		logger.info("Product expiration check job starts at: " + dateFormat.format(currentDate));
