681572b46c6c33878c45090b718d668d9852b2ea
huhaonan <huhaonan.en@gmail.com>
2016-09-30 17:03:39 +0800

    modify start.sh

--- dajia-core/start.sh
+++ dajia-core/start.sh
@@ -18,6 +18,5 @@ echo "###################################################################"
 echo ""
 echo ""
                                                      
-
-# mvn clean spring-boot:run -Drun.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000"
+mvn clean spring-boot:run -Drun.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000"
 
